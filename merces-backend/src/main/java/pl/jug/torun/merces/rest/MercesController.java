51957a91fbd7d606e041cc3c75e669e2ce9726c7
Marek Nowicki <faramir2@gmail.com>
2014-09-06 12:06:48 +0200

    third init

--- merces-backend/src/main/java/pl/jug/torun/merces/rest/MercesController.java
+++ merces-backend/src/main/java/pl/jug/torun/merces/rest/MercesController.java
@@ -6,20 +6,21 @@
 package pl.jug.torun.merces.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author faramir
  */
-//@RestController
+@RestController
 public class MercesController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-//    @RequestMapping("/greeting")
-//    public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-//        return new Greeting(counter.incrementAndGet(),
-//                String.format(template, name));
-//    }
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return String.format(template, name);
+    }
 }
